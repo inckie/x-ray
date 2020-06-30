de93d86dfd4e9392d61316c2afc2ad5ffa72c9fe
Alex <24409942+inckie@users.noreply.github.com>
2020-06-30 17:04:19 -0400

    + sinks mapping unittest

---
+++ android/xray/src/test/java/com/applicaster/xray/MapperUnitTest.kt
@@ -0,0 +1,89 @@
+package com.applicaster.xray
+
+import android.util.Log
+import com.applicaster.xray.android.routing.DefaultSinkFilter
+import com.applicaster.xray.routing.Mapper
+import com.applicaster.xray.sinks.ISink
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Testing for mapping routines
+ */
+class MapperUnitTest {
+
+    @Test
+    fun testMapperRoot() {
+        val testSinkDebug = TestSink("Debug")
+        val testSinkError = TestSink("Error")
+        Core.get()
+            .addSink("test_sink_debug", testSinkDebug)
+            .addSink("test_sink_error", testSinkError)
+            .setFilter("test_sink_debug", "", DefaultSinkFilter(Log.DEBUG))
+            .setFilter("test_sink_error", "", DefaultSinkFilter(Log.ERROR))
+
+        val debugMessage = "debug message"
+        testSinkDebug.addExpectedMessage(debugMessage)
+        Logger.get().d().message(debugMessage)
+        assertTrue("debug sink does not expects any messages", testSinkDebug.isEmpty())
+        assertTrue("error sink does not expects any messages", testSinkError.isEmpty())
+
+        val errorMessage = "error message"
+        testSinkDebug.addExpectedMessage(errorMessage)
+        testSinkError.addExpectedMessage(errorMessage)
+        Logger.get().e().message(errorMessage)
+        assertTrue("debug sink does not expects any messages", testSinkDebug.isEmpty())
+        assertTrue("error sink does not expects any messages", testSinkError.isEmpty())
+
+        Core.get().reset()
+    }
+
+    @Test
+    fun testSinksView(){
+        // validate the mapper uses sinks HashMap keys view
+        val hashMap = mutableMapOf<String, ISink>(
+            "sink0" to TestSink("Error"),
+            "sink1" to TestSink("Error")
+        )
+        val mapper = Mapper(hashMap)
+        val mappingBefore = mapper.getMapping("", "", Log.DEBUG)
+        assertEquals(mappingBefore.size, 2)
+        hashMap.remove("sink0")
+        val mappingAfter = mapper.getMapping("", "", Log.DEBUG)
+        assertEquals(mappingAfter.size, 1)
+    }
+
+    @Test
+    fun testSinksSetViewIntegration() {
+        // same as above, but through Core (some cases can not be tested via core with 100% reliability)
+        val testSink0 = TestSink("Error")
+        val testSink1 = TestSink("Error")
+
+        val testEvent = Event("tag", "subsystem", 0L, Log.DEBUG, "message", null, null)
+
+        // add mapping to not yet registered sinks
+        Core.get().setFilter("test_sink_0", "subsystem") { _, _, _ -> true }
+        Core.get().setFilter("test_sink_1", "subsystem") { _, _, _ -> true }
+
+        var mapping = Core.get().getMapping(testEvent)
+        assertTrue("No sinks available", mapping.isEmpty())
+
+        // add sinks
+        Core.get().addSink("test_sink_0", testSink0)
+        Core.get().addSink("test_sink_1", testSink1)
+
+        mapping = Core.get().getMapping(testEvent)
+        // validate mapping to contain both sinks
+        assertTrue("Sink0 was added", mapping.contains(testSink0))
+        assertTrue("Sink1 was added",mapping.contains(testSink1))
+
+        Core.get().removeSink(testSink1)
+        // validate mapping to contain only one sink
+        mapping = Core.get().getMapping(testEvent)
+        assertTrue("Sink0 is still available", mapping.contains(testSink0))
+        assertFalse("Sink1 was removed", mapping.contains(testSink1))
+
+        Core.get().reset()
+    }
+}
---
+++ android/xray/src/test/java/com/applicaster/xray/TestSink.kt
@@ -0,0 +1,28 @@
+package com.applicaster.xray
+
+import com.applicaster.xray.sinks.ISink
+import org.junit.Assert
+import java.util.ArrayDeque
+
+class TestSink(val name: String) : ISink {
+
+    private val deque = ArrayDeque<String>()
+
+    fun addExpectedMessage(message: String) {
+        deque.push(message)
+    }
+
+    override fun log(event: Event) {
+        Assert.assertFalse(
+            "Sink $name expects log output",
+            deque.isEmpty()
+        )
+        Assert.assertEquals(
+            "Sink $name expects message ${event.message}",
+            deque.pop(),
+            event.message
+        )
+    }
+
+    fun isEmpty() = deque.isEmpty()
+}
