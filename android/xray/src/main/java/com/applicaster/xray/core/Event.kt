f23e776bfbf65e230c177c185a537977f4553dac
Alex <24409942+inckie@users.noreply.github.com>
2020-07-08 12:17:09 -0400

    * Event data is immutable maps

--- android/xray/src/main/java/com/applicaster/xray/core/Event.kt
+++ android/xray/src/main/java/com/applicaster/xray/core/Event.kt
@@ -6,7 +6,7 @@ data class Event(
         val timestamp: Long, // UTC
         val level: Int,
         val message: String,
-        val data: MutableMap<String, Any>?,
-        val context: MutableMap<String, Any>?,
+        val data: Map<String, Any>?,
+        val context: Map<String, Any>?,
         val exception: Throwable? // todo: not sure about this one, maybe handle it in event builder and add to data?
 )
